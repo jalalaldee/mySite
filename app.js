ac6b194745170f1851dc26e536862a1ae2789cdc
jalalaldeen <>
2021-05-09 20:48:07 +0000

    eeee

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var age = prompt('what is your age');
 if (age >= 10 && age <= 80) {
   alert('Welcome to my site');
 } else if (age < 10 || age >= 80) {
-  msg = 'Sorry you are not allowed'
+  alert ('Sorry you are not allowed');
 } else {
   alert('Have a nice day!');
 }
