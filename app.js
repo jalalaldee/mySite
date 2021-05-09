3b0adc5e19ec5e2f6d104566690a58462214507f
jalalaldeen <>
2021-05-09 20:34:43 +0000

    pushing

---
+++ app.js
@@ -0,0 +1,12 @@
+function welcoming(){
+var age = prompt('what is your age');
+
+if (age >= 10 && age <= 80) {
+  alert('Welcome to my site');
+} else if (age < 10 || age >= 80) {
+  msg = 'Sorry you are not allowed'
+} else {
+  alert('Have a nice day!');
+}
+}
+welcoming();
